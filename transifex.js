95d29632546830200abce5852a8bb0c72073661c
Ali Al Dallal <ali@alicoding.com>
2013-10-21 11:07:00 -0400

    Backup translation

--- transifex.js
+++ transifex.js
@@ -42,8 +42,8 @@ function importFromTransifex(options) {
     if (error) {
       return console.log("Can not return the project details");
     }
-    projectDetails = JSON.parse(projectDetails);
     projectDetails = JSON.stringify(projectDetails, null, 2);
+    // projectDetails = JSON.stringify(projectDetails, null, 2);
     console.log(projectDetails)
     projectDetails.resources.forEach(function(resourcesDetails) {
       projectDetails.teams.forEach(function(entry) {
